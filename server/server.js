afa4394d1d64f23567281c4fd98843c307ff7868
Anand <Abhinav.Anand.consultant@nielsen.com>
2018-08-27 16:39:44 +0530

    chat appen function Jquery

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
+const { generateMessage } = require('./utils/message.js');
 const publicPath = path.join(__dirname,'../public');
 
 const port = process.env.PORT || 3000;
@@ -15,6 +16,8 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket) => {
   console.log('New user connected');
+  socket.emit('newMessage',generateMessage('Admin','Welcome to chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin','New user joined'));
   socket.on('disconnect',() => {
     console.log('Disconnected to server');
 });
@@ -23,14 +26,11 @@ io.on('connection',(socket) => {
 //   text:'Hey man, how is everything going',
 //   createAt: 123
 // })
-socket.on('createMessage',function(message) {
+socket.on('createMessage',function(message,callback) {
   console.log('New Email created',message);
-  io.emit('newMessage',{
-    from: message.from,
-    text: message.text,
-    createdAt: new Date().getTime()
-  })
-});
+  io.emit('newMessage', generateMessage(message.from, message.text));
+  callback('This is from the server.');
+  });
 })
 server.listen(port ,() => {
    console.log('Server is up on port 3000'); 
